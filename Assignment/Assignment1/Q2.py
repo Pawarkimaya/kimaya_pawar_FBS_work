e083d936eed35fd91be05aaf0a8e4c35e0453d6c
Kimaya Pawar <138522962+Pawarkimaya@users.noreply.github.com>
2025-11-25 00:14:46 +0530

    Add files via upload

---
+++ Assignment/Assignment1/Q2.py
@@ -0,0 +1,7 @@
+#WAP to calculate area of rectangle based on length and breadth
+x=(int(input("Enter lngth of rectangle:")))
+y=(int(input("Enter breadth of rectangle:")))
+#apply formula
+area=x*y
+#print result
+print("Area of rectangle is",area)
