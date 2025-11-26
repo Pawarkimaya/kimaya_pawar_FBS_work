5ca0e3fc9c485c9cb5221863822a3f5dded9f588
Kimaya Pawar <138522962+Pawarkimaya@users.noreply.github.com>
2025-11-26 16:58:48 +0530

    Delete Assignment/Assignment1/Q4.py

--- Assignment/Assignment1/Q4.py
+++
@@ -1,9 +0,0 @@
-#WAP  to enter P,T,R and calculate simple intrest 
-P=(float(input("Enter Principle ammount:",)))
-T=(float(input("Enter time in years:")))
-R=(float(input("Enter rate of intrest:")))
-
-#apply formula
-SI=(P*T*R)/100
-
-print("Simple intrest of given ammount is:",SI,"%")
